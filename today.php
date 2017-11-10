f25d03b7655fc83ef8e70a603a869b96201161e6
Mason <mshelby@mountvernon.org>
2017-11-10 16:53:25 -0500

    completed ux design of today at mv

--- today.php
+++ today.php
@@ -17,24 +17,50 @@ ini_set('display_errors', 1);
     <div class="uk-container uk-padding-remove">
       <?php include "includes/nav.php"; ?>
 
-      <h3 class="uk-text-uppercase uk-text-bold uk-padding-small uk-padding-remove-bottom">Today's Events</h3>
+      <h4 class="uk-text-uppercase uk-padding-small uk-padding-remove-bottom uk-padding-remove-top uk-margin-remove-top uk-margin-remove">Today's Events</h4>
 
-    <table class="uk-table uk-table-divider uk-table-small">
+    <table class="uk-table uk-table-divider uk-table-small uk-text-small">
       <thead>
           <tr>
               <th>Time</th>
               <th>Tour</th>
               <th>Location</th>
-              <th>Fee</th>
+              <th></th>
           </tr>
       </thead>
     <tbody>
         <tr>
-            <td><strong>03:45 PM</strong><br />
-              <span class="uk-label">in 1 hrs 0 min</span></td>
-            <td><strong>Premium Mansion Tour</strong></td>
+            <td>03:45PM</td>
+            <td><strong>Premium Mansion Tour</strong>
+            <span class="uk-label uk-label-warning">$</span></td>
             <td>Mansion Circle</td>
-            <td><span class="uk-label uk-label-warning">$</span></td>
+        </tr>
+        <tr uk-toggle="target: #locationid">
+            <td>04:00PM</td>
+            <td><strong>Tribute at the Tombs</strong></td>
+            <td>Tombs</td>
+        </tr>
+
+        <div id="locationid" class="uk-modal-full" uk-modal>
+            <div class="uk-modal-dialog" uk-height-viewport>
+                <button class="uk-modal-close-full uk-close-large" type="button" uk-close></button>
+                <div class="uk-padding-large">
+                    <h3>Tribute at the Tomb</h3>
+                    <p>Pay your respect to George Washington by participating in a brief wreath-laying ceremony at the Washingtons' Tomb.</p>
+                    <p class="uk-text-meta">Included with general admission, no ticket purchase is required.</p>
+                </div>
+            </div>
+        </div>
+
+        <tr>
+            <td>04:30PM</td>
+            <td><strong>Lives Bound Together</strong></td>
+            <td>Museum</td>
+        </tr>
+        <tr>
+            <td>05:00PM</td>
+            <td><strong>Miller Time</strong></td>
+            <td>Bar</td>
         </tr>
     </tbody>
 </table>
