be4c1ce35f5316cc21f35e7985f28e243c510b5b
Matt Briney <mkbriney@gmail.com>
2017-11-11 11:59:45 -0500

    Fixing slideshow mechanism by forcing v34 to load

--- includes/head.php
+++ includes/head.php
@@ -1,9 +1,9 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-<link rel="stylesheet" href="./lib/uikit/css/uikit.min.css" />
+<link rel="stylesheet" href="./lib/uikit/css/uikit.min.css?v34" />
 <link rel="stylesheet" href="./lib/ol/ol.css" type="text/css">
 <link rel="stylesheet" href="./css/style.css?v1.87" type="text/css">
-<script src="./lib/uikit/js/uikit.min.js"></script>
-<script src="./lib/uikit/js/uikit-icons.min.js"></script>
+<script src="./lib/uikit/js/uikit.min.js?v34"></script>
+<script src="./lib/uikit/js/uikit-icons.min.js?v34"></script>
 <script src="./lib/ol/ol.js"></script>
 
 <?php include "helper.php"; ?>
