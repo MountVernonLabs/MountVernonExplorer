8a02a92d0a5dbfbb4f5b2cd0961624a519f26ef1
Matt Briney <mkbriney@gmail.com>
2017-12-05 22:54:40 -0500

    UIKit Updates?

--- includes/head.php
+++ includes/head.php
@@ -1,9 +1,9 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-<link rel="stylesheet" href="./lib/uikit/css/uikit.min.css?v34" />
+<link rel="stylesheet" href="./lib/uikit/css/uikit.min.css?v35" />
 <link rel="stylesheet" href="./lib/ol/ol.css?v4.4.2" type="text/css">
 <link rel="stylesheet" href="./css/style.css?v1.90" type="text/css">
-<script src="./lib/uikit/js/uikit.min.js?v34"></script>
-<script src="./lib/uikit/js/uikit-icons.min.js?v34"></script>
+<script src="./lib/uikit/js/uikit.min.js?v35"></script>
+<script src="./lib/uikit/js/uikit-icons.min.js?v35"></script>
 <script src="./lib/ol/ol.js?v4.4.2"></script>
 
 <?php include "helper.php"; ?>
